473dfc0135b94980a5afff7a3c16dd3b9721189e
ignacio <ignacio@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-02-12 09:05:55 +0000

    Added AJAX action on widget
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/mailchimp@12857 f53fe46e-8c02-459b-9fdd-93e75771b278

---
+++ mailchimp-sync/assets/shortcode.js
@@ -0,0 +1,59 @@
+jQuery(document).ready(function($) {
+
+	var mailchimp_shortcode = {
+		init: function() {
+			var mailchimp_widgets = $( '.incsub-mailchimp-widget' );
+
+			if ( mailchimp_widgets.length ) {
+				$( '.incsub-mailchimp-widget-form' ).submit( function(e) {
+					e.preventDefault();
+
+					var elems = $(this).find('.incsub-mailchimp-field');
+
+					var form_data = {};
+					elems.each(function() {
+					    form_data[ $(this).attr("name") ] = $(this).val();
+					});
+
+					form_data['nonce'] = mailchimp_widget_captions.nonce;
+
+				  	mailchimp_widget.submit_form( form_data, $(this).attr('id') );
+				  	return false;
+				});
+			}
+		},
+		submit_form: function( form_data, form_id ) {
+			var the_form = $('#' + form_id);
+			
+			var spinner = the_form
+				.find( '.mailchimp-spinner' )
+				.css('visibility', 'visible');
+
+			$.ajax({
+				url: mailchimp_widget_captions.ajaxurl,
+				type: 'POST',
+				data: form_data
+			})
+			.done(function(return_data,xhr) {
+				$('.mailchimp-widget-error').hide();
+				if ( return_data.success ) {
+					the_form.find('*').detach();
+					var message_container = $('<p class="mailchimp-widget-updated"></p>').text(return_data.data['message']).hide();
+				}
+				else {
+					var message_container = $('<ul class="mailchimp-widget-error"></ul>').hide();
+					for ( var i = 0; i < return_data.data.length; i++ ) {
+						message_container.append( '<li>' + return_data.data[i] + '</li>' );
+					}
+				}
+				$('#' + form_id).prepend(message_container);
+				message_container.slideDown();
+
+				spinner.css( 'visibility', 'hidden' );
+			});
+			return false;
+		}
+	}
+
+	mailchimp_widget.init();
+});
--- mailchimp-sync/widget.php
+++ mailchimp-sync/widget.php
@@ -2,15 +2,15 @@
 
 class Incsub_Mailchimp_Widget extends WP_Widget {
 
-	private $errors;
+	private $errors = array();
 	private $success;
+	private $enqueue_scripts = false;
 
 	/**
 	 * Widget setup.
 	 */
 	function Incsub_Mailchimp_Widget() {
 
-		$this->errors = false;
 		$this->success = false;
 
 		/* Widget settings. */
@@ -24,6 +24,21 @@ class Incsub_Mailchimp_Widget extends WP_Widget {
 
 		add_action( 'template_redirect', array( &$this, 'validate' ) );
 
+		add_action( 'wp_ajax_incsub_mailchimp_subscribe_user', array( &$this, 'validate' ) );
+		add_action( 'wp_ajax_nopriv_incsub_mailchimp_subscribe_user', array( &$this, 'validate' ) );
+
+		add_action( 'wp_enqueue_scripts', array( &$this, 'register_scripts' ) );
+
+	}
+
+	public function register_scripts() {
+		wp_register_script( 'mailchimp-widget-js', MAILCHIMP_ASSETS_URL . 'widget.js', array( 'jquery' ), '20140212', true );
+
+		$l10n = array(
+			'ajaxurl' => admin_url( 'admin-ajax.php' ),
+			'nonce' => wp_create_nonce( "mailchimp_subscribe_user" )
+		);
+		wp_localize_script( 'mailchimp-widget-js', 'mailchimp_widget_captions', $l10n );
 	}
 
 	public function validate() {
@@ -31,8 +46,15 @@ class Incsub_Mailchimp_Widget extends WP_Widget {
 
 		if ( isset( $_POST['action'] ) && 'incsub_mailchimp_subscribe_user' == $_POST['action'] ) {
 
-			if ( ! wp_verify_nonce( $_POST['_wpnonce'], 'mailchimp_subscribe_user' ) )
-				return false;
+			$doing_ajax = defined( 'DOING_AJAX' ) && DOING_AJAX;
+
+			if ( ! $doing_ajax ) {
+				if ( ! wp_verify_nonce( $_POST['_wpnonce'], 'mailchimp_subscribe_user' ) )
+					return false;
+			}
+			else {
+				check_ajax_referer( 'mailchimp_subscribe_user', 'nonce' );
+			}
 
 			$instance = $this->get_settings();
 
@@ -41,52 +63,53 @@ class Incsub_Mailchimp_Widget extends WP_Widget {
 				
 				$email = sanitize_email( $_POST['subscription-email'] );
 				if ( ! is_email( $email ) )
-					$this->add_error( __( 'Please, insert a valid email', MAILCHIMP_LANG_DOMAIN ) );
+					$this->errors[] = ( __( 'Please, insert a valid email', MAILCHIMP_LANG_DOMAIN ) );
 
 				$firstname = sanitize_text_field( $_POST['subscription-firstname'] );
 				$firstname = ! empty( $firstname ) ? $firstname : '';
 				if ( empty( $firstname ) && $instance['require_firstname'] )
-					$this->add_error( __( 'First name is required', MAILCHIMP_LANG_DOMAIN ) );
+					$this->errors[] = ( __( 'First name is required', MAILCHIMP_LANG_DOMAIN ) );
 
 
 				$lastname = sanitize_text_field( $_POST['subscription-lastname'] );
 				$lastname = ! empty( $lastname ) ? $lastname : '';
 				if ( empty( $lastname ) && $instance['require_lastname'] )
-					$this->add_error( __( 'Last name is required', MAILCHIMP_LANG_DOMAIN ) );
+					$this->errors[] = ( __( 'Last name is required', MAILCHIMP_LANG_DOMAIN ) );
 
-				if ( ! $this->is_error() ) {
+				if ( empty( $this->errors ) ) {
 					$user['email'] = $email;
 					$user['first_name'] = $firstname;
 					$user['last_name'] = $lastname;
 
 					$mailchimp_sync->mailchimp_add_user( $user );
 
-					$link = add_query_arg( 'mailchimp-subscribed', 'true' );
-					$link .= '#incsub-mailchimp-widget-' . $this->number;
-					wp_redirect( $link );
+					
+					if ( ! $doing_ajax ) {
+						$link = add_query_arg( 'mailchimp-subscribed', 'true' );
+						$link .= '#incsub-mailchimp-widget-' . $this->number;
+						wp_redirect( $link );
+						exit;		
+					}
+					else {
+						$text = $instance['subscribed_placeholder'];
+						wp_send_json_success( array( 'message' => $text ) );
+					}
 				}
+				elseif ( ! empty( $this->errors ) && $doing_ajax ) {
+	    			wp_send_json_error( $this->errors );
+	    		}
 
 			}
 		}
 	}
-
-	private function add_error( $message ) {
-		if ( ! $this->errors )
-			$this->errors = array();
-
-		$this->errors[] = $message;
-	}
-
-	private function is_error() {
-		return ( is_array( $this->errors ) && ! empty( $this->errors ) );
-	}
-
 
 
 	/**
 	 * How to display the widget on the screen.
 	 */
 	function widget( $args, $instance ) {
+		wp_enqueue_script( 'mailchimp-widget-js' );
+
 		extract( $args );
 
         $title = apply_filters( 'widget_title', $instance['title'] );
@@ -100,19 +123,19 @@ class Incsub_Mailchimp_Widget extends WP_Widget {
 
 	    if ( isset( $_GET['mailchimp-subscribed'] ) && 'true' == $_GET['mailchimp-subscribed'] ) {
 	    	?>
-				<p style="background:#B6ECBB;border:1px solid #44793D;box-sizing:border-box;padding:5px;">
+				<p class="mailchimp-widget-updated">
 					<?php echo $instance['subscribed_placeholder']; ?>
 				</p>
 	    	<?php
 	    }
 	    else {
 		    ?>
-		        <form method="post" action="#<?php echo 'incsub-mailchimp-widget-' . $this->number; ?>" id="incsub-mailchimp-widget">
+		        <form method="post" class="incsub-mailchimp-widget-form" action="" id="incsub-mailchimp-widget-form-<?php echo $this->number; ?>">
 		        	<p>
 			        	<?php echo $text; ?>
 			        </p>
-			        <?php if ( $this->is_error() ): ?>
-			        	<ul style="background:#FFCACA;border:1px solid #B35C5C;box-sizing:border-box;list-style:none;padding:5px;">
+			        <?php if ( ! empty( $this->errors ) ): ?>
+			        	<ul class="mailchimp-widget-error">
 			        	<?php foreach ( $this->errors as $error ): ?>
 							<li style="color:#333 !important;"><?php echo $error; ?></li>
 			        	<?php endforeach; ?>
@@ -121,11 +144,37 @@ class Incsub_Mailchimp_Widget extends WP_Widget {
 		        	<input type="text" class="incsub-mailchimp-field" name="subscription-firstname" value="<?php echo isset( $_POST['subscription-firstname'] ) ? $_POST['subscription-firstname'] : ''; ?>" placeholder="<?php _e( 'First name', MAILCHIMP_LANG_DOMAIN ); ?>"><br/>
 		        	<input type="text" class="incsub-mailchimp-field" name="subscription-lastname" value="<?php echo isset( $_POST['subscription-lastname'] ) ? $_POST['subscription-lastname'] : ''; ?>" placeholder="<?php _e( 'Last name', MAILCHIMP_LANG_DOMAIN ); ?>"><br/>
 		        	<input type="email" class="incsub-mailchimp-field" name="subscription-email" value="<?php echo isset( $_POST['subscription-email'] ) ? $_POST['subscription-email'] : ''; ?>" placeholder="<?php _e( 'Email', MAILCHIMP_LANG_DOMAIN ); ?>"><br/>
-		        	<input type="hidden" name="action" value="incsub_mailchimp_subscribe_user">
+		        	<input type="hidden" class="incsub-mailchimp-field" name="action" value="incsub_mailchimp_subscribe_user">
 		        	<?php wp_nonce_field( 'mailchimp_subscribe_user' ); ?>
-		        	<input type="submit" class="incsub-mailchimp-submit" name="submit-subscribe-user" value="<?php echo $button_text; ?>">
+		        	<input type="submit" class="incsub-mailchimp-submit" name="submit-subscribe-user" value="<?php echo $button_text; ?>"> <span class="mailchimp-spinner"></span>
 		        </form>
-		        
+		        <style>
+		        	.mailchimp-spinner {
+		        		background: url('<?php echo MAILCHIMP_ASSETS_URL . "spinner.gif"; ?>') no-repeat top left transparent;
+		        		height: 25px;
+						width: 25px;
+						display: inline-block;
+						vertical-align: middle;
+						visibility:hidden;
+		        	}
+		        	.mailchimp-widget-updated {
+		        		background:#B6ECBB;
+		        		border:1px solid #44793D;
+		        		box-sizing:border-box;
+		        		padding:5px;
+		        	}
+		        	.mailchimp-widget-error {
+						background:#FFCACA;
+						border:1px solid #B35C5C;
+						box-sizing:border-box;
+						list-style:none;
+						padding:5px;
+		        	}
+		        	.incsub-mailchimp-field {
+		        		box-sizing:border-box;
+		        		width:100%;
+		        	}
+		        </style>
 	        <?php
 	    }
 		echo $after_widget;
